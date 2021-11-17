999a451c5a4aebf119ef2b6cc1ad774e45252b68
Thomas H <hutho@umich.edu>
2021-11-17 17:55:00 -0500

    added wip top scroll bar

--- src/googlemap.js
+++ src/googlemap.js
@@ -1,10 +1,106 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import key1 from './key'
-import {Map, Marker, GoogleApiWrapper} from 'google-maps-react';
+import {GoogleApiWrapper, Map, Marker} from 'google-maps-react';
 import PlacesAutocomplete, {
     geocodeByAddress,
     getLatLng,
 } from 'react-places-autocomplete';
+import {ScrollMenu, VisibilityContext} from "react-horizontal-scrolling-menu";
+
+
+const getItems = () =>{
+    return Array(20)
+        .fill(0)
+        .map((_, ind) => ({
+                id: `(fixme replace with actual date) day-${ind}`}
+        ))
+    };
+
+function Scroll() {
+    const [items, setItems] = React.useState(getItems);
+    const [selected, setSelected] = React.useState([]);
+    const [position, setPosition] = React.useState(0);
+
+    const isItemSelected = (id) => !!selected.find((el) => el === id);
+
+    const handleClick = (id) => ({ getItemById, scrollToItem }) => {
+        const itemSelected = isItemSelected(id)
+
+        setSelected((currentSelected) =>
+            itemSelected
+                ? currentSelected.filter((el) => el !== id)
+                : currentSelected.concat(id)
+        );
+    }
+
+    return (
+        <ScrollMenu
+            // LeftArrow={LeftArrow}
+            // RightArrow={RightArrow}
+        >
+            {items.map(({ id }) => (
+                <Card
+                    itemId={id} // NOTE: itemId is required for track items
+                    title={id}
+                    key={id}
+                    attraction={'effiel tower'}
+                    onClick={handleClick(id)}
+                    selected={isItemSelected(id)}
+                />)
+            )}
+
+        </ScrollMenu>
+    );
+}
+
+// function LeftArrow() {
+//     const { isFirstItemVisible, scrollPrev } = React.useContext(VisibilityContext)
+//
+//     return (
+//         <Arrow disabled={isFirstItemVisible} onClick={() => scrollPrev()}>
+//             Left
+//         </Arrow>
+//     );
+// }
+//
+// function RightArrow() {
+//     const { isLastItemVisible, scrollNext } = React.useContext(VisibilityContext)
+//
+//     return (
+//         <Arrow disabled={isLastItemVisible} onClick={() => scrollNext()}>
+//             Right
+//         </Arrow>
+//     );
+// }
+
+function Card({
+                  onClick,
+                  title,
+                attraction,
+              }) {
+    const visibility = React.useContext(VisibilityContext)
+
+    return (
+        <div
+            onClick={() => onClick(visibility)}
+            style={{
+                width: "160px",
+            }}
+            tabIndex={0}
+        >
+            <div className="card">
+                <div>{title}</div>
+                <div>attraction: {attraction}</div>
+
+            </div>
+            <div
+                style={{
+                    height: "200px",
+                }}
+            />
+        </div>
+    );
+}
 
 export class MapContainer extends Component {
     constructor(props) {
@@ -48,11 +144,14 @@ export class MapContainer extends Component {
             markers: []
         });
     }
-
+// const menu = {}
     render() {
         let marks = this.state.markers;
         return (
             <div id='googleMaps'>
+                <Scroll>
+                    {/*data={menu}*/}
+                </Scroll>
                 <PlacesAutocomplete
                     value={this.state.address}
                     onChange={this.handleChange}
