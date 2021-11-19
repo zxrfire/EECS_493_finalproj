0f4bad95437ce9b3b15799b9c11ff1e384be9475
normanqw <normanqw@umich.edu>
2021-11-19 16:34:25 -0500

    Added button to card

--- src/Day.js
+++ src/Day.js
@@ -2,8 +2,7 @@ import moment from 'moment';
 
 class Day{
   constructor(date) {
-    this.date = date;
-    this.day = moment(date).format('dddd');
+    this.date = moment(date);
     this.attractions = []
   }
 }
--- src/components/DayCard.jsx
+++ src/components/DayCard.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import {VisibilityContext} from 'react-horizontal-scrolling-menu';
-import {Card} from 'react-bootstrap';
+import {Card, ListGroup, Button} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import moment from 'moment';
+
 
 const DayCard = (props) => {
   const visibility = React.useContext(VisibilityContext);
   const { day } = props;
 
-  // const getCardTitle = ()=>{
-    // retur
-  // };
+  const getCardTitle = ()=>{
+    // format in english form
+    return day.date.format('LL');
+  };
 
   return (
       // <Card.Title
@@ -18,9 +22,24 @@ const DayCard = (props) => {
       //     }}
       //     tabIndex={0}
       // >
-        <Card>
-          <Card.Title>{day.date}</Card.Title>
-        </Card>
+     <React.Fragment>
+       <Card style={{ width: '18rem' }}>
+         <Card.Body>
+           <Card.Title>{getCardTitle()}</Card.Title>
+           <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+           <Card.Text>
+             Some quick example text to build on the card title and make up the bulk of
+             the card's content.
+           </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+         </Card.Body>
+         <ListGroup variant="flush">
+           <ListGroup.Item>Cras justo odio</ListGroup.Item>
+           <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+           <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+         </ListGroup>
+       </Card>
+     </React.Fragment>
         // <div
         //     style={{
         //       height: "200px",
