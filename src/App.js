39b569e95e8457641078b43316f6ca04a3e077a6
normanqw <normanqw@umich.edu>
2021-11-19 13:37:50 -0500

    Preparing to merge

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import Map from './googlemap.js'
+import Map from './components/googlemap.js'
 import {GoogleApiWrapper} from "google-maps-react";
-import MyComponents from "./googlemap.js";
+import MyComponents from "./components/googlemap.js";
 import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
 import ReactGoogleAutocomplete from "react-google-autocomplete";
 import key from "./key";
 import React from "react";
 import Autocomplete from "react-google-autocomplete";
-import GoogleMap from './googlemap';
+import GoogleMap from './components/googlemap';
 
 function App() {
 
