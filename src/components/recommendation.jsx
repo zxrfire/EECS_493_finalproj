e034c38b0f033d1adb01efe70b50d6da022740e4
normanqw <normanqw@umich.edu>
2021-11-28 17:24:20 -0500

    Refactored r

--- src/components/recommendation.jsx
+++ src/components/recommendation.jsx
@@ -1,95 +1,47 @@
-import { distanceAndSkiddingToXY } from '@popperjs/core/lib/modifiers/offset';
-import React, {Component} from 'react';
-import PlacesAutocomplete, {
-    geocodeByAddress,
-    getLatLng,
-  } from 'react-places-autocomplete';
+import React from 'react';
 //import { GoogleApiWrapper, InfoWindow, Map, Marker } from 'google-maps-react'
 //import key1 from '../key';
 
 
-class Recommendation extends Component {
-    constructor(props) {
-        super(props);
-        // this.state = {
-            // attractions: [],
-            // mapCenter: {
-            //     lat: -91,
-            //     lng: -181,
-            // },
-        // };
-        this.createTable = this.createTable.bind(this);
-        this.getRecommendations();
-    }
+const Recommendation = props => {
 
-    getRecommendations = () => {
+
+    const getRecommendations = () => {
         let service = new window.google.maps.places.PlacesService(document.createElement('div'));
-        service.textSearch({query: this.props.cityObj.formatted_address + 'points of interest'},
+        service.textSearch({query: props.cityObj.formatted_address + 'points of interest'},
             (results, status) => {
                 if (status === window.google.maps.places.PlacesServiceStatus.OK) {
                     console.log(results);
-                    this.props.newRecommendations(results);
+                    props.newRecommendations(results);
                 }
             }
         );
     };
 
-    // getLocationObject = address => geocodeByAddress(address);
+    getRecommendations();
 
-    // determineCoordinates(newGeoObj) {
-    //     getLatLng(newGeoObj)
-    //     .then(latLng => {
-    //         console.log('Determined coordinates:', latLng);
-    //         // update center state
-    //         this.setState({mapCenter: latLng});
-    //     })
-    //     .then(() => {
-    //         let centre = new window.google.maps.LatLng(this.state.mapCenter.lat, this.state.mapCenter.lng);
-    //         let service = new window.google.maps.places.PlacesService(document.createElement('div'));
-    //         service.textSearch({
-    //             query: this.props.cityObj.formatted_address + 'points of interest'
-    //             /*location: centre,
-    //             radius: 1000,
-    //             rankby: 'distance',
-    //             type: 'point_of_interest'*/
-    //         }, (results, status) => {
-    //             if (status === window.google.maps.places.PlacesServiceStatus.OK) {
-    //                 console.log(results);
-    //                 this.setState({
-    //                     attractions: results,
-    //                 });
-    //             }
-    //         });
-    //     })
-    //     .catch(error => console.error('Error', error));
-    // }
+    const createTable = () => {
+        const {recommendations} = props;
+        return <table style={{width: '100%'}}>
+            <caption style={{captionSide: "top"}}>Recommended Attractions
+                for {props.cityObj.formatted_address}</caption>
+            <tr>
+                <th>#</th>
+                <th>Attraction</th>
+            </tr>
+            {recommendations.map((attraction, index) => <tr>
+                <td>{index})</td>
+                <td>{attraction.name}</td>
+            </tr>)
+            }
+        </table>;
+    };
 
-    createTable() {
-        const {recommendations} = this.props;
-        let table = 
-            <table style={{width:'100%'}}>
-                <caption style={{captionSide: "top"}}>Recommended Attractions for {this.props.cityObj.formatted_address}</caption>
-                <tr>
-                    <th>#</th>
-                    <th>Attraction</th>
-                </tr>
-                {recommendations.map((attraction, index) => <tr>
-                    <td>{index})</td>
-                    <td>{attraction.name}</td>
-                </tr>)
-                }
-            </table>;
-        return table;
-    }
-
-    render() {
-        let table = this.createTable();
-        return (
-            <div>
-                {table}
-            </div>
-        );
-    }
-}
+    return (
+        <div>
+            {createTable()}
+        </div>
+    );
+};
 
 export default Recommendation;
