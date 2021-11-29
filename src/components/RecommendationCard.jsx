566499887d60a34526adfaf125452a45c41114be
normanqw <normanqw@umich.edu>
2021-11-29 00:27:12 -0500

    Added image

--- src/components/RecommendationCard.jsx
+++ src/components/RecommendationCard.jsx
@@ -20,16 +20,18 @@ const RecommendationCard = props =>{
         <Accordion.Item eventKey={attractionId}>
           <Accordion.Header className={"card shadow bg-body rounded"}
                  ref={drag}>
-              <div className={"d-flex justify-content-between align-items-center text-center"}
+              <div className={"d-flex justify-content-between align-items-center "}
                    data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                {attraction.name}
+                <span className={"text-center"}>{attraction.name}</span>
               </div>
           </Accordion.Header>
           <Accordion.Body>
             <Row>
-              <Col xs={3}><span className="oi oi-home"></span></Col>
-              <Col xs={9}></Col>
-              <h7>{attraction.formatted_address}</h7>
+              <img src={attraction.photos[0].getUrl()} />
+            </Row>
+            <Row>
+              <Col xs={1} className={"d-flex align-items-center justify-content-center"}><span className="align-middle oi oi-home"></span></Col>
+              <Col xs={11}><h7>{attraction.formatted_address}</h7></Col>
             </Row>
           </Accordion.Body>
         </Accordion.Item>
