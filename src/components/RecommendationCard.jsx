19f5a3ae6b14585c6ff61ddad14e404d907b4ac4
normanqw <normanqw@umich.edu>
2021-11-29 01:50:20 -0500

    ratings

--- src/components/RecommendationCard.jsx
+++ src/components/RecommendationCard.jsx
@@ -4,6 +4,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {useDrag} from 'react-dnd';
 import {Accordion, Row, Col} from 'react-bootstrap';
 import 'open-iconic/font/css/open-iconic-bootstrap.css';
+import key from '../key';
+import Rating from 'react-rating';
+import axios from 'axios';
 
 const RecommendationCard = props =>{
 
@@ -20,17 +23,56 @@ const RecommendationCard = props =>{
   }));
 
 
-  const handleCollapse = () => {
-    if (attraction.photos){
-      setURL(attraction.photos[0].getUrl());
-    }
-  };
+  // const handleCollapse = () => {
+  //   if (attraction.photos){
+  //     setURL(attraction.photos[0].getUrl());
+  //   }
+  //   axios.get("https://maps.googleapis.com/maps/api/place/details/json&place_id={attraction.place_id}&key=${key}`
+  // };
 
   const renderImage = () => {
+    return (url !== "" &&
+        <Row className={"mb-2"}>
+          <img src={url} />
+        </Row>);
+  };
+
+  const renderRatings = () => {
+    let ratings;
+    if (attraction.rating){
+      ratings =
+          <Fragment>
+            <Col xs={7} className={"d-flex align-items-center justify-content-start"}>
+              <Rating
+                fractions={10}
+                initialRating={attraction.rating}
+                readonly
+            /> </Col>
+            <Col xs={3} className={"ms-auto"}>
+              <span  className={"align-middle"}>{attraction.rating}/5</span>
+            </Col>
+          </Fragment>;
+    } else {
+      ratings = <Col xs={11}>Ratings Not Available</Col>;
+    }
     return (
-            <Row>
-              <img src={url} />
-            </Row>);
+            <Row className={"mt-1 align-content-center"}>
+              <Col xs={1} className={"d-flex align-items-center justify-content-center"}>
+                <span className="oi oi-star"></span>
+              </Col>
+              {ratings}
+            </Row>
+    );
+  };
+
+  const renderHours = () => {
+    return (
+    <Row className={"mt-1 align-content-center"}>
+      <Col xs={1} className={"d-flex align-items-center justify-content-center"}>
+        <span className="oi oi-clock"></span>
+      </Col>
+      <Col xs={11}>attraction.</Col>
+    </Row>);
   };
 
   return (
@@ -44,7 +86,8 @@ const RecommendationCard = props =>{
               </div>
           </Accordion.Header>
           <Accordion.Body>
-            {url !== "" && renderImage()}
+            {renderImage()}
+            {renderRatings()}
             <Row className={"mt-1"}>
               <Col xs={1} className={"d-flex align-items-center justify-content-center"}><span className="align-middle oi oi-home"></span></Col>
               <Col xs={11}><h7>{attraction.formatted_address}</h7></Col>
