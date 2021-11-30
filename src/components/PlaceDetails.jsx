1c9a880f83da8980ac6c0a7988f3f102ff2163cc
normanqw <normanqw@umich.edu>
2021-11-30 15:56:46 -0500

    Added more rec info

--- src/components/PlaceDetails.jsx
+++ src/components/PlaceDetails.jsx
@@ -42,10 +42,61 @@ const PlaceDetails = props => {
   };
 
   const renderAddress = () => {
-    return (<Row className={"mt-1"}>
-      <Col xs={1} className={"d-flex align-items-center justify-content-center"}><span className="align-middle oi oi-home"></span></Col>
+    return (place.formatted_address && <Row className={"mt-1"}>
+      <Col xs={1} className={"d-flex align-items-center justify-content-center"}>
+        <span className="oi oi-location"></span>
+      </Col>
       <Col xs={11}><h6>{place.formatted_address}</h6></Col>
     </Row>);
+  };
+
+  const renderPhone = () => {
+    return (place.international_phone_number && <Row className={"mt-1"}>
+      <Col xs={1} className={"d-flex align-items-center justify-content-center"}>
+        <span className="oi oi-phone"></span>
+      </Col>
+      <Col xs={11} className={"d-flex align-items-center"}><h6>{place.international_phone_number}</h6></Col>
+    </Row>);
+  };
+
+  const renderOpenHours = () => {
+    if (place['opening_hours'] && place['opening_hours']['weekday_text']){
+      return (
+          <Fragment>
+            <Row className={"mt-1"}>
+              <Col xs={1} className={"mt-1  d-flex align-items-start   justify-content-center"}>
+                <span className="oi oi-clock"></span>
+              </Col>
+              <Col xs={11} >
+              {place.opening_hours.weekday_text.map(
+                  hours => (<Row>
+                    <h6>{hours}</h6>
+                  </Row>)
+              )}</Col>
+            </Row>
+
+          </Fragment>
+      );
+    }
+  };
+
+  const renderWebsite = () => {
+    if (place['website']){
+      return (
+      <Row className={"mt-1"}>
+        <Col xs={1} className={"mt-3  d-flex align-items-start   justify-content-center"}>
+          <span className="oi oi-external-link"></span>
+        </Col>
+        <Col xs={11}>
+          <a href={place['website']}
+             target={"_blank"}
+             rel="noopener noreferrer"
+                  className={"btn btn-outline-secondary"}>
+            Visit the Attraction's Website
+          </a>
+        </Col>
+      </Row>);
+    }
   };
 
   return (
@@ -53,6 +104,9 @@ const PlaceDetails = props => {
         {renderImage()}
         {renderRatings()}
         {renderAddress()}
+        {renderOpenHours()}
+        {renderPhone()}
+        {renderWebsite()}
       </Fragment>
   );
 };
